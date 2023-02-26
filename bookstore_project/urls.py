427d4590b6b51de71743a303321941eaec989ef4
Chhabii <achrxn@gmail.com>
2023-02-26 21:18:05 +0545

    ch12

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #django admin
@@ -15,4 +17,4 @@ urlpatterns = [
     
     path("",include("pages.urls")),
     path("books/",include("books.urls")),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

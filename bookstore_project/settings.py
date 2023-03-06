f2744c12065c3ddb2405d692f5b0f70348768ce7
Chhabii <achrxn@gmail.com>
2023-03-06 20:33:00 +0545

    ch19

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    'whitenoise.runserver_nostatic',
     "django.contrib.staticfiles",
 
     "django.contrib.sites", #new
@@ -96,6 +97,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 MIDDLEWARE = [
     'django.middleware.cache.UpdateCacheMiddleware',
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -231,3 +233,8 @@ STRIPE_TEST_SECRET_KEY=config('STRIPE_TEST_SECRET_KEY')
 # hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 # INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
 INTERNAL_IPS = ['127.0.0.1']
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['DEFAULT'].update(db_from_env)
